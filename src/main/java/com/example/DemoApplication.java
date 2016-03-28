099a34009aa135bb38d6338c22821e84d501e8f9
Gabby <guobingwei@aliyun.com>
2016-03-28 14:58:02 +0800

    加入博客模板

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -6,15 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @EnableAutoConfiguration
 @SpringBootApplication
 public class DemoApplication {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	String home() {
-		return "Hello World!";
+	ModelAndView home() {
+		ModelAndView modelAndView = new ModelAndView("/blog/type1/index.html");
+		return modelAndView;
 	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
